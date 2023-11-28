ee030167c59b8d7777279c6a2a319d3f38b85bc2
Santiago Saavedra <santiagosaav.99@gmail.com>
2023-11-28 11:46:11 -0300

    add id and password directly

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ class DefaultConfig:
     """ Bot Configuration """
     
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "143ebc27-dff8-4fdf-b3c6-c33374b54dfa")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "bQb8Q~GMqMGpJToXANLoZxF-44AHY1lW4isYFb0A")
