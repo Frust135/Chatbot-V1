7172956ed11ed48f0c54b78fd00e67be092f2f22
Santiago Saavedra <santiagosaav.99@gmail.com>
2023-11-30 13:30:26 -0300

    cards incorporation and first try of form

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
 from typing import List
 from botbuilder.core import (
     ActivityHandler,
-    TurnContext
+    TurnContext,
+    CardFactory
 )
 
 from botbuilder.schema import (
     ChannelAccount,
     Activity,
     ActivityTypes,
-    SuggestedActions,
-    CardAction
+    CardAction,
+    HeroCard,
+    Attachment
 )
 
 class Bot(ActivityHandler):
@@ -17,27 +19,90 @@ class Bot(ActivityHandler):
     async def on_message_activity(self, turn_context: TurnContext):
         text = turn_context.activity.text.lower()
         
-        if "options" in text:
-            await self.send_options(turn_context)
-        elif "option" in text:
-            await turn_context.send_activity(
-                "Selected option: {}".format(turn_context.activity.text)
-            )
+        if "formulario" in text:
+            await self.send_form(turn_context)
         else:
-            await turn_context.send_activity("Hello World!")
+            await self.send_options(turn_context)
+    
+    async def send_form(self, turn_context: TurnContext):
+
+        fields = [
+            {
+                "label": "Nombre:",
+                "type": "text",
+                "id": "nombre"
+            },
+            {
+                "label": "Apellido:",
+                "type": "text",
+                "id": "apellido"
+            },
+            {
+                "label": "Correo electrónico:",
+                "type": "text",
+                "id": "correo"
+            },
+            {
+                "label": "Mensaje:",
+                "type": "text",
+                "id": "mensaje"
+            }
+        ]
+
+        card = HeroCard(
+            title="Formulario",
+            subtitle="Por favor, rellene los siguientes campos:",
+            buttons=[
+                CardAction(type="imBack", title="Enviar formulario", value="submit")
+            ],
+            items=[
+                {
+                    "type": "AdaptiveCard",
+                    "content": {
+                        "type": "VerticalCard",
+                        "items": [
+                            {
+                                "type": "TextRun",
+                                "text": field["label"]
+                            },
+                            {
+                                "type": "Input.TextInput",
+                                "id": field["id"],
+                                "placeholder": "Introduzca su respuesta",
+                                "style": "block"
+                            }
+                        ]
+                    }
+                } for field in fields
+            ]
+        )
+
+        attachment = Attachment(content_type=CardFactory.content_types.hero_card, content=card.__dict__)
+
+        message = Activity(
+            type=ActivityTypes.message,
+            text="Por favor, complete el siguiente formulario:",
+            attachments=[attachment]
+        )
+
+        await turn_context.send_activity(message)
+
+    
     
     async def send_options(self, turn_context: TurnContext):
-        options = ["Option 1", "Option 2", "Option 3"]
-        
-        actions = [
-            CardAction(type="imBack", title=option, value=option) for option in options
-        ]        
-        suggest = SuggestedActions(actions=actions)
+        options = ["Formulario"]
+        card = HeroCard(
+            title="Opciones",
+            buttons=[
+               CardAction(type="imBack", title=option, value=option) for option in options
+            ]
+        )
+        attachment = Attachment(content_type=CardFactory.content_types.hero_card, content=card.__dict__)
         
         message = Activity(
             type=ActivityTypes.message,
-            text="Select an option:",
-            suggested_actions=suggest
+            text="Selecciona una opción:",
+            attachments=[attachment]
         )
         
         await turn_context.send_activity(message)
@@ -45,4 +110,4 @@ class Bot(ActivityHandler):
     async def on_members_added_activity(self, members_added: List[ChannelAccount], turn_context: TurnContext):
         for member_added in members_added:
             if member_added.id != turn_context.activity.recipient.id:
-                await turn_context.send_activity("Hello and welcome!")
+                await self.send_options(turn_context)
