f018295ab03dad5077e9f99f1134962c860fb737
Santiago Saavedra <santiagosaav.99@gmail.com>
2023-12-07 11:08:38 -0300

    Generic API function with params

--- bot/utils/generic_api_handler.py
+++ bot/utils/generic_api_handler.py
@@ -1,24 +1,48 @@
 import requests
-from .apis import APIS_GOLIAT
 
-async def call_api(api_name, params):
-    api_info = APIS_GOLIAT.get(api_name)
-    if api_info:
-        url = api_info["url"]
-        method = api_info["method"]
-        if method == "GET":
-            response = requests.get(url, params=params)
-        elif method == "POST":
-            response = requests.post(url, data=params)
-        else:
-            response = None
-        data = response.json()
-        return data
-    else:
-        return {"error": "API not found"}
+URL = "https://goliat.falabella.cl/api"
+
+
+async def call_api(type_api, function, action, params):
+    """
+    Calls an API endpoint of Falabella with the specified parameters.
+
+    Args:
+        type_api (str): The type of API.
+        function (str): The API function to call.
+        action (str): The HTTP action to perform (GET or POST).
+        params (list): The list of parameters to include in the API call.
+
+    Returns:
+        dict: The JSON response from the API.
+
+    Raises:
+        Exception: If the API call fails with a non-200 status code.
     
-async def handle_option(option_name, params):
-    api_name = f"api_{option_name}"
-    result = await call_api(api_name, params)
-    print(result)
-    # Here we can display it as a card, or show the info, idk
+    Example:
+    json_response = await call_api(
+        type_api="awx",
+        function="get_inventories",
+        action="GET",
+        params=[
+            {"name": "search_pattern", "value": "GoLiAtInventory_202"}
+        ,
+    )
+    """
+    api_url = f"{URL}/{type_api}/v1/{function}"
+    for index, param in enumerate(params):
+        aux_text = "?" if index == 0 else "&"
+        api_url += f"{aux_text}{param['name']}={param['value']}"
+    json_response = None
+    if action == "GET":
+        response = requests.get(api_url)
+    elif action == "POST":
+        response = requests.post(api_url)
+    if response.status_code == 200:
+        json_response = response.json()
+        return json_response
+    else:
+        print(f"Error en la solicitud: {response.status_code}")
+        print(f"URL: {api_url}")
+        print(f"Response: {response.text}")
+        raise Exception(f"Error en la solicitud: {response.status_code}")
