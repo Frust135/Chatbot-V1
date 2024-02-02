2ee8bc6a5fae4775d85b65c79a5ec5b49f778f27
Santiago Saavedra <mssr.99@hotmail.com>
2024-02-02 10:11:51 -0300

    modified credentials

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class DefaultConfig:
     
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "143ebc27-dff8-4fdf-b3c6-c33374b54dfa")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", ".ZD8Q~bKMHTa2nsV5EWhDRoJSzm1bnerwSl1_c_h")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "bQb8Q~GMqMGpJToXANLoZxF-44AHY1lW4isYFb0A")
