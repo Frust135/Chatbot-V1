f008499ff0f7b1ab0b3c235d7218b9959ce726a5
Santiago Saavedra <mssr.99@hotmail.com>
2024-02-02 11:28:26 -0300

    Changed credentials and return logger

--- bot/bot.py
+++ bot/bot.py
@@ -34,9 +34,9 @@ class Bot(ActivityHandler):
         value or text and then calling the appropriate handler function.
         """
         text = turn_context.activity.text
-        # try:
-        #     member = await TeamsInfo.get_member(turn_context, turn_context.activity.from_property.id)
-        #     logger.info(f"Member: {member}")
-        # except Exception as e:
-        #     logger.error(f"Error getting member: {e}")
+        try:
+            member = await TeamsInfo.get_member(turn_context, turn_context.activity.from_property.id)
+            logger.info(f"Member: {member}")
+        except Exception as e:
+            logger.error(f"Error getting member: {e}")
         await turn_context.send_activity(text)
--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class DefaultConfig:
     
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "143ebc27-dff8-4fdf-b3c6-c33374b54dfa")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "bQb8Q~GMqMGpJToXANLoZxF-44AHY1lW4isYFb0A")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "EH38Q~u-CMPVBDBwfxndK1MfW-xHTJQFhxfP~aXj")
